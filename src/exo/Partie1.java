744917a7807bc579c469c3375e97d1b45f44b4f9
Nolan Guillemain <nolanmateo05@gmail.com>
2026-05-06 10:04:44 +0200

    Partie 1 filtre avec streams(long, bad, recent trips)

--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,43 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Partie1 {
 
+    // Lambda Exercice 1 : trajet long ET cher
+    private final Predicate<Trip> isLongAndExpensive = trip ->
+            trip.distanceKm() > 10 && trip.price() > 20;
+
+    // Lambda Exercice 2 : mauvais trajet
+    private final Predicate<Trip> isBadTrip = trip ->
+            trip.rating() < 3;
+
+    // Lambda Exercice 3 : trajet d'aujourd'hui ou d'hier
+    private final Predicate<Trip> isRecentTrip = trip -> {
+        LocalDate tripDate = trip.startTime().toLocalDate();
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+
+        return tripDate.equals(today) || tripDate.equals(yesterday);
+    };
+
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
-        // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+                .filter(isLongAndExpensive)
+                .toList();
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
-        // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(isBadTrip)
+                .toList();
     }
-
     public List<Trip> recentTrips(List<Trip> trips) {
-        // aujourd’hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(isRecentTrip)
+                .toList();
     }
 }
